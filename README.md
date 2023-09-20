44a8c45d00411f3e900030acff7c3d430492e1f9
Maximous Black <maximousblk@gmail.com>
2023-09-20 08:55:10 +0530

    feat: deno deploy compatible
    + use hono framework
    + upgrade simplewebauthn to v8
    + make constants configurable through env
    + remove npm deps
    
    FIX #1 (Update to support v8 and deno deploy)

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Passkeys Demo
 
 This is a simple demo of [Passkeys Authentication](https://passkeys.dev/) using
-[SimpleWebAuthn](https://simplewebauthn.dev/) and
-[Deno](https://deno.com/runtime). Usernameless authentication is supported.
+[SimpleWebAuthn](https://simplewebauthn.dev/) and [Deno](https://deno.com/runtime). Usernameless authentication is
+supported.
 
 ## Requirements
 
@@ -11,7 +11,6 @@ This is a simple demo of [Passkeys Authentication](https://passkeys.dev/) using
 ## Running the demo
 
 0. Star this repo 🌟
-1. Clone this repo using
-   `git clone https://github.com/maximousblk/passkeys-demo.git`
+1. Clone this repo using `git clone https://github.com/maximousblk/passkeys-demo.git`
 2. Run `deno task start` in the cloned directory to start the server
 3. Open [`http://localhost`](http://localhost:80) in your browser
--- server.ts
+++ server.ts
@@ -1,10 +1,29 @@
-import SimpleWebAuthnServer from 'npm:@simplewebauthn/server@7.2.0';
-import { jwtVerify, SignJWT } from 'https://deno.land/x/jose@v4.14.4/index.ts';
+import {
+  generateAuthenticationOptions,
+  generateRegistrationOptions,
+  verifyAuthenticationResponse,
+  verifyRegistrationResponse,
+} from "https://deno.land/x/simplewebauthn@v8.1.1/deno/server.ts";
+import type {
+  AuthenticationResponseJSON,
+  RegistrationResponseJSON,
+} from "https://deno.land/x/simplewebauthn@v8.1.1/deno/typescript-types.ts";
+import { jwtVerify, SignJWT } from "https://deno.land/x/jose@v4.14.6/index.ts";
+import { Hono } from "https://deno.land/x/hono@v3.6.3/mod.ts";
+import { getSignedCookie, logger, serveStatic, setSignedCookie } from "https://deno.land/x/hono@v3.6.3/middleware.ts";
 
-const SECRET = new TextEncoder().encode('development');
+// CONSTANTS
+
+const SECRET = new TextEncoder().encode(Deno.env.get("JWT_SECRET") ?? "development");
+const DATABASE = Deno.env.get("DENO_DEPLOYMENT_ID") ? undefined : "users.db";
+const RP_ID = Deno.env.get("WEBAUTHN_RP_ID") ?? "localhost";
+const RP_NAME = Deno.env.get("WEBAUTHN_RP_NAME") ?? "Deno Passkeys Demo";
+const CHALLENGE_TTL = Number(Deno.env.get("WEBAUTHN_CHALLENGE_TTL")) || 60_000;
+
+// UTILS
 
 function generateJWT(userId: string) {
-  return new SignJWT({ userId }).setProtectedHeader({ alg: 'HS256' }).sign(SECRET);
+  return new SignJWT({ userId }).setProtectedHeader({ alg: "HS256" }).sign(SECRET);
 }
 
 function verifyJWT(token: string) {
@@ -17,16 +36,16 @@ function generateRandomID() {
   return btoa(
     Array.from(id)
       .map((c) => String.fromCharCode(c))
-      .join('')
+      .join(""),
   )
-    .replace(/\+/g, '-')
-    .replace(/\//g, '_')
-    .replace(/=/g, '');
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_")
+    .replace(/=/g, "");
 }
 
 // DATABASE
 
-const kv = await Deno.openKv('./users.db');
+const kv = await Deno.openKv(DATABASE);
 
 type User = {
   username: string;
@@ -40,213 +59,189 @@ type Credential = {
   counter: number;
 };
 
-type Challenge = {
-  timeout: number;
-};
+type Challenge = true;
 
 // RP SERVER
 
-const homepage = await Deno.readTextFile('index.html');
+const app = new Hono();
 
-const handler: Deno.ServeHandler = async (req) => {
-  const url = new URL(req.url);
+app.use("*", logger());
 
-  switch (url.pathname) {
-    case '/': {
-      return new Response(homepage, {
-        headers: { 'content-type': 'text/html' },
-      });
-    }
+app.get("/", serveStatic({ path: "./index.html" }));
 
-    case '/register/options': {
-      console.log(req.method, req.url);
+app.post("/register/options", async (c) => {
+  const { username } = await c.req.json<{ username: string }>();
+  console.log({ username });
 
-      const json = await req.json();
-      console.log({ json });
+  const userID = generateRandomID();
 
-      const userID = generateRandomID();
+  const options = await generateRegistrationOptions({
+    rpName: RP_NAME,
+    rpID: RP_ID,
+    userID,
+    userName: username,
+    userDisplayName: username,
+    authenticatorSelection: {
+      residentKey: "required",
+      userVerification: "required",
+      authenticatorAttachment: "platform",
+    },
+  });
 
-      const options = SimpleWebAuthnServer.generateRegistrationOptions({
-        rpName: 'Passkeys Demo',
-        rpID: req.headers.get('host')!,
-        userID,
-        userName: json.username,
-        userDisplayName: json.username,
-        authenticatorSelection: {
-          residentKey: 'required',
-          userVerification: 'required',
-          authenticatorAttachment: 'platform',
-        },
-      });
+  console.log({ options });
 
-      console.log({ options });
+  await kv.set(["challenges", options.challenge], true, {
+    expireIn: CHALLENGE_TTL,
+  });
 
-      await kv.set(['challenges', options.challenge], { timeout: Date.now() + 60_000 } as Challenge);
+  await setSignedCookie(c, "userId", userID, SECRET, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "Strict",
+    path: "/",
+    maxAge: CHALLENGE_TTL,
+  });
 
-      return new Response(JSON.stringify(options), {
-        headers: {
-          'content-type': 'application/json',
-          'set-cookie': `userId=${userID}; HttpOnly; Secure; SameSite=Strict; Path=/; Expires=${new Date(Date.now() + 60_000).toUTCString()}`,
-        },
-      });
-    }
+  return c.json(options);
+});
 
-    case '/register/verify': {
-      console.log(req.method, req.url);
+app.post("/register/verify", async (c) => {
+  const { username, cred } = await c.req.json<{ username: string; cred: RegistrationResponseJSON }>();
+  console.log({ username, cred });
 
-      const json = await req.json();
-      console.log({ json });
+  const userId = await getSignedCookie(c, SECRET, "userId");
+  if (!userId) return new Response("Unauthorized", { status: 401 });
+  console.log({ userId });
 
-      const cookie = req.headers.get('cookie');
-      if (!cookie) return new Response('Unauthorized', { status: 401 });
-      console.log({ cookie });
+  const clientData = JSON.parse(atob(cred.response.clientDataJSON));
+  console.log({ clientData });
 
-      const userId = cookie
-        .split(';')
-        .find((c) => c.trim().startsWith('userId='))
-        ?.split('=')[1];
-      if (!userId) return new Response('Unauthorized', { status: 401 });
-      console.log({ userId });
+  const challenge = await kv.get<Challenge>(["challenges", clientData.challenge]);
+  console.log({ challenge });
 
-      const clientData = JSON.parse(atob(json.cred.response.clientDataJSON));
-      console.log({ clientData });
-
-      const challenge = await kv.get<Challenge>(['challenges', clientData.challenge]);
-      console.log({ challenge });
-
-      if (!challenge.value || challenge.value.timeout < Date.now()) {
-        return new Response('Challenge expired', { status: 400 });
-      }
-
-      const verification = await SimpleWebAuthnServer.verifyRegistrationResponse({
-        response: json.cred,
-        expectedChallenge: clientData.challenge,
-        expectedRPID: req.headers.get('host')!,
-        expectedOrigin: req.headers.get('origin')!,
-        requireUserVerification: true,
-      });
-      console.log({ verification });
-
-      if (verification.verified) {
-        const { credentialID, credentialPublicKey, counter } = verification.registrationInfo!;
-
-        await kv.delete(['challenges', clientData.challenge]);
-
-        await kv.set(['users', userId], {
-          username: json.username,
-          data: "Demo user's data",
-          credentials: {
-            [json.cred.id]: {
-              credentialID,
-              credentialPublicKey,
-              counter,
-            },
-          },
-        } as User);
-
-        return new Response(JSON.stringify(verification), {
-          headers: {
-            'content-type': 'application/json',
-            'set-cookie': `token=${await generateJWT(userId)}; HttpOnly; Secure; SameSite=Strict; Path=/; Expires=${new Date(Date.now() + 600_000).toUTCString()}`,
-          },
-        });
-      }
-
-      return new Response('Unauthorized', { status: 401 });
-    }
-
-    case '/login/options': {
-      console.log(req.method, req.url);
-
-      const options = SimpleWebAuthnServer.generateAuthenticationOptions({
-        userVerification: 'required',
-        rpID: req.headers.get('host')!,
-      });
-
-      console.log({ options });
-
-      await kv.set(['challenges', options.challenge], { timeout: Date.now() + 60_000 } as Challenge);
-
-      return new Response(JSON.stringify(options), {
-        headers: { 'content-type': 'application/json' },
-      });
-    }
-
-    case '/login/verify': {
-      console.log(req.method, req.url);
-
-      const json = await req.json();
-      console.log({ json });
-
-      const clientData = JSON.parse(atob(json.cred.response.clientDataJSON));
-      console.log({ clientData });
-
-      const userId = json.cred.response.userHandle;
-      const user = await kv.get<User>(['users', userId]);
-      if (!user.value) return new Response('Unauthorized', { status: 401 });
-      console.log({ user });
-
-      const challenge = await kv.get<Challenge>(['challenges', clientData.challenge]);
-      if (!challenge.value || challenge.value.timeout < Date.now()) {
-        return new Response('Challenge expired', { status: 400 });
-      }
-
-      const verification = await SimpleWebAuthnServer.verifyAuthenticationResponse({
-        response: json.cred,
-        expectedChallenge: clientData.challenge,
-        expectedOrigin: 'http://localhost',
-        expectedRPID: 'localhost',
-        authenticator: user.value.credentials[json.cred.id],
-      });
-
-      if (verification.verified) {
-        const { newCounter } = verification.authenticationInfo!;
-
-        await kv.delete(['challenges', clientData.challenge]);
-
-        await kv.set(['users', userId], {
-          ...user.value,
-          credentials: {
-            ...user.value.credentials,
-            [json.cred.id]: {
-              ...user.value.credentials[json.cred.id],
-              counter: newCounter,
-            },
-          },
-        } as User);
-
-        return new Response(JSON.stringify(verification), {
-          headers: {
-            'content-type': 'application/json',
-            'set-cookie': `token=${await generateJWT(userId)}; HttpOnly; Secure; SameSite=Strict; Path=/; Expires=${new Date(Date.now() + 600_000).toUTCString()}`,
-          },
-        });
-      }
-
-      return new Response('Unauthorized', { status: 401 });
-    }
-
-    case '/private': {
-      console.log(req.method, req.url);
-
-      const cookie = req.headers.get('cookie');
-      const token = cookie?.split('=')[1];
-      if (!token) return new Response('Unauthorized', { status: 401 });
-      console.log({ token });
-
-      const result = await verifyJWT(token);
-      console.log({ result });
-
-      const user = await kv.get<User>(['users', result.payload.userId as string]);
-      if (!user.value) return new Response('Unauthorized', { status: 401 });
-
-      return new Response(user.value.data);
-    }
-
-    default: {
-      return new Response('Not found', { status: 404 });
-    }
+  if (!challenge.value) {
+    return c.text("Invalid challenge", 400);
   }
-};
 
-Deno.serve({ port: 80, handler });
+  const verification = await verifyRegistrationResponse({
+    response: cred,
+    expectedChallenge: clientData.challenge,
+    expectedRPID: RP_ID,
+    expectedOrigin: c.req.header("origin")!, //! Allow from any origin
+    requireUserVerification: true,
+  });
+  console.log({ verification });
+
+  if (verification.verified) {
+    const { credentialID, credentialPublicKey, counter } = verification.registrationInfo!;
+
+    await kv.delete(["challenges", clientData.challenge]);
+
+    await kv.set(["users", userId], {
+      username: username,
+      data: "Demo user's data",
+      credentials: {
+        [cred.id]: {
+          credentialID,
+          credentialPublicKey,
+          counter,
+        },
+      },
+    } as User);
+
+    await setSignedCookie(c, "token", await generateJWT(userId), SECRET, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "Strict",
+      path: "/",
+      maxAge: 600_000,
+    });
+
+    return c.json(verification);
+  }
+
+  return c.text("Unauthorized", 401);
+});
+
+app.post("/login/options", async (c) => {
+  const options = await generateAuthenticationOptions({
+    userVerification: "required",
+    rpID: RP_ID,
+  });
+
+  console.log({ options });
+
+  await kv.set(["challenges", options.challenge], true, {
+    expireIn: CHALLENGE_TTL,
+  });
+
+  return c.json(options);
+});
+
+app.post("/login/verify", async (c) => {
+  const { cred } = await c.req.json<{ cred: AuthenticationResponseJSON }>();
+  console.log({ cred });
+
+  const clientData = JSON.parse(atob(cred.response.clientDataJSON));
+  console.log({ clientData });
+
+  const userId = cred.response.userHandle;
+  console.log({ userId });
+  if (!userId) return c.text("Unauthorized", 401);
+
+  const user = await kv.get<User>(["users", userId]);
+  if (!user.value) return c.text("Unauthorized", 401);
+  console.log({ user });
+
+  const challenge = await kv.get<Challenge>(["challenges", clientData.challenge]);
+  if (!challenge.value) {
+    return c.text("Invalid challenge", 400);
+  }
+
+  const verification = await verifyAuthenticationResponse({
+    response: cred,
+    expectedChallenge: clientData.challenge,
+    expectedOrigin: c.req.header("origin")!, //! Allow from any origin
+    expectedRPID: RP_ID,
+    authenticator: user.value.credentials[cred.id],
+  });
+
+  if (verification.verified) {
+    const { newCounter } = verification.authenticationInfo;
+
+    await kv.delete(["challenges", clientData.challenge]);
+
+    const newUser = user.value;
+    newUser.credentials[cred.id].counter = newCounter;
+
+    await kv.set(["users", userId], newUser);
+
+    await setSignedCookie(c, "token", await generateJWT(userId), SECRET, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "Strict",
+      path: "/",
+      maxAge: 600_000,
+    });
+
+    return c.json(verification);
+  }
+
+  return c.text("Unauthorized", 401);
+});
+
+app.get("/private", async (c) => {
+  const token = await getSignedCookie(c, SECRET, "token");
+  if (!token) return new Response("Unauthorized", { status: 401 });
+  console.log({ token });
+
+  const result = await verifyJWT(token);
+  console.log({ result });
+
+  const user = await kv.get<User>(["users", result.payload.userId as string]);
+  if (!user.value) return new Response("Unauthorized", { status: 401 });
+
+  return c.text(user.value.data);
+});
+
+Deno.serve({ port: 80 }, app.fetch);
